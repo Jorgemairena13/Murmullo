f6e537c3013ee4a49ffa1a83fee0cee580bc216d
Jorge <jorgemairena13@gmail.com>
2026-05-23 11:41:27 +0200

    fix: add CORS config for Vercel frontend

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: ['api/*']);
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
+    
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (AuthenticationException $e, Request $request) {
             if ($request->is('api/*') || $request->expectsJson()) {
